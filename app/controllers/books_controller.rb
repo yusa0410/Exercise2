e85be964a085ddb4b47d6bfa47dc29bdb06a4d7b
yusa0410 <yusaku0410ishida@gmail.com>
2021-05-03 10:02:40 +0000

    提出用

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_current_user, {only: [:edit,:update,:destroy]}
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
+    @user = current_user
     @books = Book.all
     @book = Book.new
   end
@@ -17,6 +22,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      flash[:notice] = "errors prohibited this obj from being saved."
       render 'index'
     end
   end
@@ -36,16 +42,27 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
+  end
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+def  ensure_current_user
+  @book = Book.find(params[:id])
+  if @book.user_id != current_user.id
+    redirect_to books_path
+  end
+end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, {only: [:edit,:update,:destroy]}
 
   def show
     @user = User.find(params[:id])
-    @books = Book.all
+    @books = @user.books
     @book = Book.new
   end
 
   def index
     @users = User.all
+    @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def edit
@@ -21,6 +24,7 @@ class UsersController < ApplicationController
       flash[:notice] = "You have updated user successfully."
       redirect_to "/users/#{current_user.id}"
     else
+      flash[:notice] = "errors prohibited this obj from being saved."
       render "edit"
     end
   end
@@ -28,6 +32,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
   def ensure_correct_user
